330355debe6ac5267e4967f3c8ad11e40b586f19
maxmiliano milicic <maximiliano.milicic@gmail.com>
2023-02-12 16:24:52 -0300

    punto seguro models

---
+++ AppCoder/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from AppCoder.models import Curso
+
+# Create your views here.
+def curso(self):
+    curso = Curso(nombre="Desarrollo web", comision=1982)
+    curso.save()
+    documentosDeTexto = f'-----> Curso: {curso.nombre} comision: {curso.comision}'
+    return HttpResponse(documentosDeTexto)
+
